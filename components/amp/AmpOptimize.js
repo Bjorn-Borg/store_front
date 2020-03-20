11930aedaec06e8d9234c174a1858cddd84d86d5
Andre Tauffer <andregiraotauffer@gmail.com>
2020-03-20 15:38:34 +0100

    Added analytics script

---
+++ components/amp/AmpOptimize.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AmpIncludeCustomElement from "./AmpIncludeCustomElement";
+
+export default props => (
+  <>
+    <AmpIncludeCustomElement name="amp-experiment" version="0.1" />
+    <amp-experiment type={props.type}>
+      {props.script && (
+        <script
+          type="application/json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(props.script)
+          }}
+        />
+      )}
+    </amp-experiment>
+  </>
+);
