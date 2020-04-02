e09a5e4e6083762b4a8bdf9eb08921ea006c54fd
Andre Tauffer <andregiraotauffer@gmail.com>
2020-04-02 11:06:40 +0200

    Testing commit hook

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 //   handleChange: (event: React.MouseEvent<HTMLElement>) => void;
 //   classes: { root: string };
 // }
+const test: string = "test12s2sd2222122";
 
 const Nav = () => (
   <nav>
