6dfd2f3a28ac3bf6d88e498ca80cd048c5757b70
Andre Tauffer <andregiraotauffer@gmail.com>
2020-04-02 10:53:26 +0200

    Testing commit hook

--- components/bottomNavigation.tsx
+++ components/bottomNavigation.tsx
@@ -13,7 +13,7 @@ interface TypedProps {
   classes: { root: string };
 }
 
-const test: string = "test12s2sd2";
+const test: string = "test12s2sd22";
 
 const BottomNav = (props: TypedProps) => {
   const { value, handleChange, classes } = props;
